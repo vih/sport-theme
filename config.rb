9b3254c6c0807cb4c0816cc16e0ff6172fd5ef21
Lars Olesen <lars@intraface.dk>
2013-09-17 21:27:45 +0200

    Now using image-url for all the images

--- config.rb
+++ config.rb
@@ -11,7 +11,7 @@ fonts_dir = "fonts"
 output_style = :compact
 
 # To enable relative paths to assets via compass helper functions. Uncomment:
-# relative_assets = true
+relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
 # line_comments = false
