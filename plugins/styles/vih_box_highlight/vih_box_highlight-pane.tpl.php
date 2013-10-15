3711d8322c67b0e8c643bc1cf46941e1588ecf35
Lars Olesen <lars@intraface.dk>
2013-10-15 11:59:55 +0200

    Moved the css for panels around and introduced base for the scss

--- plugins/styles/vih_box_highlight/vih_box_highlight-pane.tpl.php
+++ plugins/styles/vih_box_highlight/vih_box_highlight-pane.tpl.php
@@ -1,5 +1,5 @@
 <?php
-drupal_add_css(drupal_get_path('module','vih_styles') .'/plugins/styles/vih_box_highlight/vih_box_highlight.css', array('group' => CSS_DEFAULT, 'every_page' => TRUE)); ?>
+drupal_add_css(drupal_get_path('theme','sport') .'/css/styles/vih_box_highlight.css', array('group' => CSS_DEFAULT, 'every_page' => TRUE)); ?>
 <div class="vih-box-highlight-pane">
 <?php if (isset($content->title)): ?>
 <h2 class="pane-title <?php print (isset($settings['top_color'])) ? $settings['top_color'] : 'blue'; ?>"><?php print $content->title; ?></h2>
