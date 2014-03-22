ac5a12c594e93b6225f70bf462d09817b45c31c1
Lars Olesen <lars@intraface.dk>
2014-03-22 15:16:33 +0000

    Do not include line comments anymore and make sure image is not wider than content area

--- config.rb
+++ config.rb
@@ -15,7 +15,7 @@ output_style = :compact
 relative_assets = true
 
 # To disable debugging comments that display the original location of your selectors. Uncomment:
-# line_comments = false
+line_comments = false
 color_output = false
 
 
