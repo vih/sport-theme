7959fda09fd5b385258dcdb1e22f5ace156574bd
lsolesen <lars@intraface.dk>
2014-10-27 19:44:26 +0000

    Minor adjustments

--- Gemfile
+++ Gemfile
@@ -4,3 +4,4 @@ source "https://rubygems.org"
 gem 'compass', '1.0.1'
 gem 'sass', '3.4.0'
 gem 'breakpoint', '2.5.0'
+gem 'scss-lint', '0.29.0'
--- Gruntfile.js
+++ Gruntfile.js
@@ -35,14 +35,26 @@ module.exports = function(grunt) {
         }
       }
     },
+    // Lint Sass sources
+    scsslint: {
+      allFiles: [
+          'scss/*/*.scss'
+        ],
+      options: {
+        config: 'config/scss-lint.yml',
+        bundleExec: true
+      }
+    }
   });
 
   // Load the plugin
   grunt.loadNpmTasks('grunt-contrib-watch');
   grunt.loadNpmTasks('grunt-contrib-compass');
   grunt.loadNpmTasks('grunt-styleguide');
+  grunt.loadNpmTasks('grunt-scss-lint');
 
   // Default task(s).
   grunt.registerTask('default', ['compass', 'watch']);
+  grunt.registerTask('validate:sass', ['scsslint']);
 
 };
